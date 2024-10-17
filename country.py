abf8eff6dc6c5cf0bcc43104f53ac1d4b59e4cda
josef.fu <josef.fu@gmail.com>
2024-10-17 15:04:21 +0800

    add currency.py

---
+++ country.py
@@ -0,0 +1,17 @@
+from countryinfo import CountryInfo
+
+country = CountryInfo(input("Enter the country name: "))
+
+print("Country Name:", country.name())
+print("Capital:", country.capital())
+print("Population:", country.population())
+print("Area (in square kilometers):", country.area())
+print("Region:", country.region())
+print("Subregion:", country.subregion())
+print("Demonym:", country.demonym())
+print("Currency:", country.currencies())
+print("Languages:", country.languages())
+print("Borders:", country.borders())
+print("Flag:", country.flag())
+# print("Timezone:", country.timezones())
+# print("Translations:", country.translations())
